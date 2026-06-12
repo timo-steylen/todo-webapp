781c6389dac1b6d1e35b760e3d7d3e4916ad3cec
Timo Steylen <Timo.Steylen@kpn.com>
2026-06-12 12:25:18 +0200

    implement todo-create

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ cd frontend
 npm install
 ng serve
 
-DB-Admin: http://localhost:8081 (server: postgres, user: todo, password: todo, database: webapp)
+DB-Admin: http://localhost:8085 (server: postgres, user: todo, password: todo, database: webapp)
 
 Swagger: http://localhost:8080/swagger-ui/index.html
--- frontend/src/app/pages/todo-create/todo-create.ts
+++ frontend/src/app/pages/todo-create/todo-create.ts
@@ -1,9 +1,13 @@
-import {Component} from '@angular/core';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+import {MatButtonModule} from '@angular/material/button';
+import {TodoService} from '../../services/todo-service';
+import {CreateTodoRequest} from '../../models/create-todo-request.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-todo-create',
@@ -14,12 +18,56 @@ import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/m
     MatFormFieldModule,
     MatDatepickerInput,
     MatDatepickerToggle,
-    MatDatepicker
+    MatDatepicker,
+    MatButtonModule,
   ],
   templateUrl: './todo-create.html',
   styleUrl: './todo-create.scss',
 })
 export class TodoCreate {
-  readonly date = new FormControl(new Date());
-  readonly serializedDate = new FormControl(new Date().toISOString());
+
+  private fb = inject(FormBuilder);
+  private todoService = inject(TodoService);
+  private router = inject(Router);
+
+  todoForm = this.fb.nonNullable.group({
+    name: ['', Validators.required],
+    description: [''],
+    tags: [''],
+    deadlineDate: [null as Date | null]
+  });
+
+  saveTodo(): void {
+    const formValue = this.todoForm.getRawValue();
+
+    if (this.todoForm.invalid) {
+      console.log("Invalid Todo form: " + JSON.stringify(this.todoForm.getRawValue()));
+      this.todoForm.markAllAsTouched();
+      return;
+    }
+    console.log(formValue.deadlineDate);
+
+    const request: CreateTodoRequest = {
+      name: formValue.name,
+      description: formValue.description,
+      tags: formValue.tags,
+      deadlineDate: formValue.deadlineDate
+        ? formValue.deadlineDate.toISOString().split('T')[0]
+        : undefined
+    };
+
+    this.todoService.createTodo(request).subscribe({
+      next: savedTodo => {
+        console.log('To-do successfully saved: ', savedTodo);
+        this.router.navigate(['/']);
+      },
+      error: error => {
+        console.error('Error during saving: ', error);
+      }
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/todos']);
+  }
 }
