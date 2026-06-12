e59a6d5681858862e31a247b2f519587c46e2134
Timo Steylen <timo.steylen@gmail.com>
2026-06-12 20:30:23 +0200

    first try todo-update

--- frontend/src/app/pages/todo-update/todo-update.ts
+++ frontend/src/app/pages/todo-update/todo-update.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from '@angular/material/card';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {MatChip, MatChipSet} from '@angular/material/chips';
+import {MatSlideToggle} from '@angular/material/slide-toggle';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-todo-update',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardContent,
+    MatCheckbox,
+    MatCardActions,
+    MatChipSet,
+    MatChip,
+    MatSlideToggle,
+    MatButton
+  ],
   templateUrl: './todo-update.html',
   styleUrl: './todo-update.scss',
 })
