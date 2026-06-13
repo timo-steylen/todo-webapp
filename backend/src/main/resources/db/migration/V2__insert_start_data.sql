abf42bdfa65b88ce1ead13dd47337b066a8023d3
Timo Steylen <timo.steylen@gmail.com>
2026-06-13 17:38:03 +0200

    add test data for first use

---
+++ backend/src/main/resources/db/migration/V2__insert_start_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO todo (name, description, tags, deadline_date, completed, created_at)
+VALUES
+    ('Webapp afronden', 'Laatste wijzigingen doorvoeren en opsturen', 'Java, Angular, Postgres', '2026-06-15', false, now()),
+    ('Angular', 'Todo detail pagina maken', 'study', '2026-06-21', false, now()),
+    ('Flyway updaten', 'Flyway migratie file maken met test data', 'Postgres, Flyway', '2026-06-14', false, now()),
+    ('Java', 'Java backend afronden', 'Java, Spring', '2026-06-14', false, now());
